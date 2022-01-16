c385113dbdfb45744f4d01c89b5f4595024a039b
alamshertiwana <45050712+alamshertiwana@users.noreply.github.com>
2022-01-16 10:54:16 +0000

    Added config.js file and updated index.html

---
+++ app/config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyBhqI5gPXI7UNbUiswgvTRC3wnD6SELM9c",
+authDomain: "reviewsystem-b2602.firebaseapp.com",
+projectId: "reviewsystem-b2602",
+storageBucket: "reviewsystem-b2602.appspot.com",
+messagingSenderId: "639222450302",
+appId: "1:639222450302:web:7dcc07b601af8bcc63f02f"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
