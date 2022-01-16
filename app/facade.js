6893463e8889e187fee86943d19df6309752b85f
alamshertiwana <45050712+alamshertiwana@users.noreply.github.com>
2022-01-16 19:00:04 +0000

    Updated Facade

--- app/facade.js
+++ app/facade.js
@@ -3,4 +3,61 @@ class userFacade {
     constructor() {
     }
 
+    getRestaurants(){
+     
+    }
+    
+    filterRestaurants(){
+
+    }
+    
+    loadMoreRestaurants(){
+
+    }
+    
+    showUserDetails(ID){
+ 
+    }
+    
+    showUserReviews(ID){  
+        
+    }
+     
+     checkUserInDB = function(user){
+    
+    }
+      
+    openRestaurant(ID) {
+
+
+    }
+        
+    addRestaurantReview(resturant_ID,restaurant_name){
+    
+    
+    }     
+
+} 
+
+class adminFacade {
+
+    constructor() {
+    }
+
+    addRestaurant(){
+        
+    }
+
+    removeReviewText = function(ID,element){   
+
+    }
+
+    toggleRestaurant (ID,hidden,element){        
+    
+    }    
+    
+    isAdmin(){
+       
+    }       
+
 } 
